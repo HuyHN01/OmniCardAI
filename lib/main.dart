8d6b05e9e0d25923a5c2ba22ce24eab7ed2bfccf
HuyHN01 <167284534+HuyHN01@users.noreply.github.com>
2026-01-21 09:42:51 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:omni_card_ai/core/theme/app_theme.dart';
 import 'package:omni_card_ai/core/routes/route_config.dart';
-import 'package:omni_card_ai/presentation/deck_detail/pages/deck_detail_screen.dart';
-import 'package:omni_card_ai/presentation/study/pages/completion_page.dart';
-import 'package:omni_card_ai/presentation/study/pages/study_screen.dart';
 
 const String APPLICATION_NAME = 'OmniCard AI';
 
@@ -16,15 +13,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return MaterialApp.router(
-    //   title: APPLICATION_NAME,
-    //   theme: AppTheme.lightTheme,
-    //   routerConfig: goRouter,
-    //   debugShowCheckedModeBanner: false,
-    // );
-
-    return MaterialApp(
-      home: CompletionPage(),
+    return MaterialApp.router(
+      title: APPLICATION_NAME,
+      theme: AppTheme.lightTheme,
+      routerConfig: goRouter,
+      debugShowCheckedModeBanner: false,
     );
+
+    // return MaterialApp(
+    //   //home: DeckDetailScreen(),
+    //   home: StudyScreen(),
+    // );
   }
 }
